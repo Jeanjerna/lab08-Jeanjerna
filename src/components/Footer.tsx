a4805259329bf4b287f3248e94a7621aaa0a8bc7
Jeanjerna <chaichanchaisak2549@gmail.com>
2025-08-01 18:48:00 +0700

    2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { type FooterProps } from "../libs/types"
-export function Footer(prop:FooterProps) {
+
+export default function Footer(prop:FooterProps) {
   return (
     <footer className="text-secondary text-center p-2 bg-light mt-auto">
       Copyright © {prop.year} {prop.fullName} {prop.studentId}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,4 @@
-import type App from "../App";
-
-export function Header() {
+export default function Header() {
   return (
     <header className="text-white bg-primary p-2 w-100">
       <h4>Note App</h4>
